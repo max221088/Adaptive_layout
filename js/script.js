ba731b1d6ad22041fd721a61ec79bab597ad1658
max221088 <max.work221088@gmail.com>
2023-02-02 02:26:09 +0200

    add adaptive whith media

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-minimal").addEventListener("click", function () {
+	document.querySelector(".menu_item_mini").classList.toggle("active");
+	document.querySelector(".menu-minimal").classList.toggle("active_btn");
+});
